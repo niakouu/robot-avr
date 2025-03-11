6185628278f40880249c96db892036859380534c
Edelina Alieva <edelina.alieva@polymtl.ca>
2025-03-11 10:17:16 -0400

    feat(tp7): MovementManager.cpp implmented.

--- tp/tp7/lib/src/MovementManager.cpp
+++ tp/tp7/lib/src/MovementManager.cpp
@@ -6,37 +6,65 @@ template class MovementManager<uint8_t, TimerPrescalerAsynchronous>;
 
 template <typename T, typename U>
 MovementManager<T, U>::MovementManager(const Timer<T, U>& timer,
-                                       Motor&& motorLeft, Motor&& motorRight)
+                                       const Motor&& motorLeft,
+                                       const Motor&& motorRight)
     : timer_(timer), motorLeft_(motorLeft), motorRight_(motorRight) {}
 
 template <typename T, typename U>
 MovementManager<T, U>::~MovementManager() {
-    // TODO
+    this->stop();
 }
 
 template <typename T, typename U>
 void MovementManager<T, U>::moveForward(float speed) const {
-    // TODO
+    this->timer_.setAsPwm(
+        {.prescaler = U::Value::CLK_DIV_8,
+         .ratioA = static_cast<T>(this->motorLeft_.move(speed, true)),
+         .ratioB = static_cast<T>(this->motorRight_.move(speed, true)),
+         .compareOutputModeA = TimerCompareOutputModeA::CLEAR,
+         .compareOutputModeB = TimerCompareOutputModeB::CLEAR});
 }
 
 template <typename T, typename U>
 void MovementManager<T, U>::moveBackward(float speed) const {
-    // TODO
+    this->timer_.setAsPwm(
+        {.prescaler = U::Value::CLK_DIV_8,
+         .ratioA = static_cast<T>(this->motorLeft_.move(speed, false)),
+         .ratioB = static_cast<T>(this->motorRight_.move(speed, false)),
+         .compareOutputModeA = TimerCompareOutputModeA::CLEAR,
+         .compareOutputModeB = TimerCompareOutputModeB::CLEAR});
 }
 
 template <typename T, typename U>
 void MovementManager<T, U>::moveLeft(float speed, float curveRatio) const {
-    // TODO
+    float ratioLeft = this->motorLeft_.move(speed, true);
+    this->timer_.setAsPwm(
+        {.prescaler = U::Value::CLK_DIV_8,
+         .ratioA = static_cast<T>(ratioLeft),
+         .ratioB = static_cast<T>(ratioLeft * curveRatio),
+         .compareOutputModeA = TimerCompareOutputModeA::CLEAR,
+         .compareOutputModeB = TimerCompareOutputModeB::CLEAR});
 }
 
 template <typename T, typename U>
 void MovementManager<T, U>::moveRight(float speed, float curveRatio) const {
-    // TODO
+    float ratioRight = this->motorRight_.move(speed, true);
+    this->timer_.setAsPwm(
+        {.prescaler = U::Value::CLK_DIV_8,
+         .ratioA = static_cast<T>(ratioRight * curveRatio),
+         .ratioB = static_cast<T>(ratioRight),
+         .compareOutputModeA = TimerCompareOutputModeA::CLEAR,
+         .compareOutputModeB = TimerCompareOutputModeB::CLEAR});
 }
 
 template <typename T, typename U>
 void MovementManager<T, U>::stop() const {
-    // TODO
+    this->timer_.setAsPwm(
+        {.prescaler = U::Value::CLK_DIV_8,
+         .ratioA = 0,
+         .ratioB = 0,
+         .compareOutputModeA = TimerCompareOutputModeA::CLEAR,
+         .compareOutputModeB = TimerCompareOutputModeB::CLEAR});
 }
 
 template <typename T, typename U>
