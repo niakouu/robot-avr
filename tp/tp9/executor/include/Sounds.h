c67f417655a33e3b4bc5214d386241be09e1212a
Kyle Bouchard <kylecbouchard@gmail.com>
2025-03-23 19:58:44 -0400

    fix(tp9): multiply delays

--- tp/tp9/executor/include/Sounds.h
+++ tp/tp9/executor/include/Sounds.h
@@ -6,18 +6,18 @@
 namespace sounds {
     struct MidiNote {
         uint8_t note;
-        uint8_t sustainTimeMs;
-        uint8_t silenceTimeMs;
+        uint16_t sustainTimeMs;
+        uint16_t silenceTimeMs;
     };
 
-    constexpr const MidiNote WINDOWS_BOOT[] = {{75, 12, 0}, {63, 6, 0},
-                                               {70, 18, 0}, {68, 24, 0},
-                                               {75, 12, 0}, {70, 36, 0}};
+    constexpr const MidiNote WINDOWS_BOOT[] = {{75, 300, 0}, {63, 150, 0},
+                                               {70, 450, 0}, {68, 600, 0},
+                                               {75, 300, 0}, {70, 900, 0}};
 
-    constexpr const MidiNote WINDOWS_SHUTDOWN[] = {{80, 12, 0},
-                                                   {75, 12, 0},
-                                                   {68, 18, 0},
-                                                   {70, 18, 0}};
+    constexpr const MidiNote WINDOWS_SHUTDOWN[] = {{80, 300, 0},
+                                                   {75, 300, 0},
+                                                   {68, 450, 0},
+                                                   {70, 450, 0}};
 } // namespace sounds
 
 #endif /* _SOUNDS_H */
