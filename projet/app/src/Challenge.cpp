826bfe58abdf873d044e0dac523f976828f5ca87
ilbelb <gigl@localhost.localdomain>
2025-04-01 11:39:03 -0400

    feat(fix): Modified Challenge.cpp

--- projet/app/src/Challenge.cpp
+++ projet/app/src/Challenge.cpp
@@ -1,1 +1,31 @@
 #include "Challenge.h"
+
+Challenge::Challenge() : currentState_(State::INITIALIZATION) {}
+
+void Challenge::startChallenge(){
+
+}
+void Challenge::initiazliationHandler(){
+
+}
+void Challenge::followLineHandler(){
+
+}
+void Challenge::locateHandler(){
+
+}
+void Challenge::forkChallengeHandler(){
+
+}
+void Challenge::houseChallengeHandler(){
+
+}
+void Challenge::mazeChallengeHandler(){
+
+}
+void Challenge::parkHandler(){
+
+}
+void Challenge::finishHandler(){
+
+}
