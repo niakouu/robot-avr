db434a0495be6f1fe9bf9ca1cd6a51ff52952a2a
Kyle Bouchard <kylecbouchard@gmail.com>
2025-04-04 09:55:07 -0400

    feat(projet): add challenge handler

---
+++ projet/app/include/ChallengeHandler.h
@@ -0,0 +1,14 @@
+#ifndef _CHALLENGE_HANDLER_H
+#define _CHALLENGE_HANDLER_H
+
+#include <stdint.h>
+
+class ChallengeHandler
+{
+public:
+    virtual void enter() = 0;
+
+    virtual void update(uint16_t deltaTimeMs) = 0;
+};
+
+#endif /* _CHALLENGE_HANDLER_H */
